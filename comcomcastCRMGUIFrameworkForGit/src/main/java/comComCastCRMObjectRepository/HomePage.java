44ba0980cac440b38770ddb36d0cc7cf587f6c3d
Amrutha <Amrutha@AmruthaM>
2024-09-02 10:32:52 +0530

    Merge branch 'master' of https://github.com/Chandrikacc/ComcastFrameworkRepo.git

--- comcomcastCRMGUIFrameworkForGit/src/main/java/comComCastCRMObjectRepository/HomePage.java
+++ comcomcastCRMGUIFrameworkForGit/src/main/java/comComCastCRMObjectRepository/HomePage.java
@@ -7,44 +7,43 @@ import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 	WebDriver driver;
-	public HomePage(WebDriver driver){
-		this.driver=driver;
-		PageFactory.initElements(driver,this);
+
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
-	
+
 	@FindBy(linkText = "Organizations")
 	private WebElement organizationsLink;
-	
-	@FindBy(linkText = "Contacts")
-	private WebElement contactLink;
-	
-	@FindBy(xpath = "//img[@src='themes/softed/images/user.PNG']")
-	private WebElement logoutIcon;
-	
-	@FindBy(linkText = "Sign Out" ) 
-	private WebElement signoutLink;
 
 	public WebElement getOrganizationsLink() {
 		return organizationsLink;
 	}
 
+	@FindBy(linkText = "Contacts")
+	private WebElement contactLink;
+
 	public WebElement getContactLink() {
 		return contactLink;
 	}
+
+	@FindBy(xpath = "//img[@src='themes/softed/images/user.PNG']")
+	private WebElement logoutIcon;
 
 	public WebElement getLogoutIcon() {
 		return logoutIcon;
 	}
 
-	public WebElement getSinoutLink() {
+	@FindBy(linkText = "Sign Out")
+	private WebElement signoutLink;
+
+	public WebElement getSignoutLink() {
 		return signoutLink;
 	}
-	
+
 	public void logoutFromApplication() {
 		logoutIcon.click();
-		signoutLink.click();	
+		signoutLink.click();
 	}
-	
 
 }
-
