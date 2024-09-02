500a441e4b03e695c39abae293495021d6d00283
Amrutha <Amrutha@AmruthaM>
2024-09-02 12:29:43 +0530

    // baseclass added

--- comcomcastCRMGUIFrameworkForGit/src/main/java/comComCastCRMGenericBaseTest/BaseClass.java
+++ comcomcastCRMGUIFrameworkForGit/src/main/java/comComCastCRMGenericBaseTest/BaseClass.java
@@ -1,6 +1,6 @@
 package comComCastCRMGenericBaseTest;
 
-// baseclass
+// baseclass added
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
